ae9e0d92d17b023aed2c4181195cb56b8d81e928
Fausto Saporito <fausto.saporito@gmail.com>
2023-10-16 20:42:07 +0000

    added more OPS

--- CMP6/in80/c.c
+++ CMP6/in80/c.c
@@ -119,14 +119,14 @@ void i0(void)
     }
     for(int i=0;i<2*WSIZE;i++) {
         SP[i]=0;
-        ONE16[i]=0;
+        ONES16[i]=0;
         ONES16[i]=1;
     }
     for(int i=0;i<MSIZE;i++) {
         M[i]=calloc(WSIZE,sizeof(char));
     }
     ONE[WSIZE-1]=1;
-    ONE16[2*WSIZE-1]=1;
+    ONES16[2*WSIZE-1]=1;
     PC=0;
     FL[6]=1;
 }
@@ -159,7 +159,7 @@ void _sub(word *r, word* ss, word *c)
     FL[5]=cnt1(r);
 }
 
-void _add16(word *r, word *sh, word *sl, word *c)
+void _add16(word *r, word *sh, word *sl, word *c, int rv)
 {
     word s[2*WSIZE];
     s[0]=sh[0];
@@ -189,22 +189,24 @@ void _add16(word *r, word *sh, word *sl, word *c)
     if(isz(r)) FL[1]=1; else FL[1]=0;
     FL[5]=cnt1(r);
 
-    sh[0]=s[0];
-    sh[1]=s[1];
-    sh[2]=s[2];
-    sh[3]=s[3];
-    sh[4]=s[4];
-    sh[5]=s[5];
-    sh[6]=s[6];
-    sh[7]=s[7];
-    sl[0]=s[8];
-    sl[1]=s[9];
-    sl[2]=s[10];
-    sl[3]=s[11];
-    sl[4]=s[12];
-    sl[5]=s[13];
-    sl[6]=s[14];
-    sl[7]=s[15];
+    if (rv) {
+        sh[0]=s[0];
+        sh[1]=s[1];
+        sh[2]=s[2];
+        sh[3]=s[3];
+        sh[4]=s[4];
+        sh[5]=s[5];
+        sh[6]=s[6];
+        sh[7]=s[7];
+        sl[0]=s[8];
+        sl[1]=s[9];
+        sl[2]=s[10];
+        sl[3]=s[11];
+        sl[4]=s[12];
+        sl[5]=s[13];
+        sl[6]=s[14];
+        sl[7]=s[15];
+    }
 }
 
 void _daa(void)
@@ -229,11 +231,31 @@ void exc(word *a)
     word oFL[WSIZE];
     switch(b2u(a))
     {
-        case 0:
-        case 8:
+        case 0x00:
+        case 0x08:
         case 0x10:
         case 0x20:
         case 0x30:
+        case 0x40:
+        case 0x49:
+        case 0x52:
+        case 0x5B:
+        case 0x64:
+        case 0x6D:
+        case 0x7F:
+            PC++;
+            break;
+        case 0x50:
+            mv(D,B);
+            PC++;
+            break;
+        case 0x60:
+            mv(H,B);
+            PC++;
+            break;
+        case 0x70:
+            ad=b2u16(H,L);
+            mv(M[ad],B);
             PC++;
             break;
         case 1:
@@ -276,6 +298,23 @@ void exc(word *a)
 
             PC+=3;
             break;
+        case 0x41:
+            mv(B,C);
+            PC++;
+            break;
+        case 0x51:
+            mv(D,C);
+            PC++;
+            break;
+        case 0x61:
+            mv(H,C);
+            PC++;
+            break;
+        case 0x71:
+            ad=b2u16(H,L);
+            mv(M[ad],C);
+            PC++;
+            break;
         case 2:
             // STAX
             ad=b2u16(B,C);
@@ -286,6 +325,32 @@ void exc(word *a)
             // STAX
             ad=b2u16(D,E);
             mv(M[ad],A);
+            PC++;
+            break;
+        case 0x22:
+            // SHLD
+            ad=b2u16(M[PC+2],M[PC+1]);
+            mv(M[ad],L);
+            mv(M[ad+1],H);
+            PC+=3;
+            break;
+        case 0x32:
+            // STA
+            ad=b2u16(M[PC+2],M[PC+1]);
+            mv(A,M[ad]);
+            PC+=3;
+            break;
+        case 0x42:
+            mv(B,D);
+            PC++;
+            break;
+        case 0x62:
+            mv(H,D);
+            PC++;
+            break;
+        case 0x72:
+            ad=b2u16(H,L);
+            mv(M[ad],D);
             PC++;
             break;
         case 3:
@@ -315,8 +380,25 @@ void exc(word *a)
         case 0x33:
             // INX
             mv(oFL,FL);
-            _add16(SP,ONE16,&c);
+            _add16(SP,ONES,ONES,&c,0);
             mv(FL,oFL);
+            PC++;
+            break;
+        case 0x43:
+            mv(B,E);
+            PC++;
+            break;
+        case 0x53:
+            mv(D,E);
+            PC++;
+            break;
+        case 0x63:
+            mv(H,E);
+            PC++;
+            break;
+        case 0x73:
+            ad=b2u16(H,L);
+            mv(M[ad],E);
             PC++;
             break;
         case 4:
@@ -347,6 +429,19 @@ void exc(word *a)
             FL[7]=oFL[7];
             PC++;
             break;
+        case 0x44:
+            mv(B,H);
+            PC++;
+            break;
+        case 0x54:
+            mv(D,H);
+            PC++;
+            break;
+        case 0x74:
+            ad=b2u16(H,L);
+            mv(M[ad],H);
+            PC++;
+            break;
         case 5:
             // DCR
             mv(oFL,FL);
@@ -375,6 +470,23 @@ void exc(word *a)
             FL[7]=oFL[7];
             PC++;
             break;
+        case 0x45:
+            mv(B,L);
+            PC++;
+            break;
+        case 0x55:
+            mv(D,L);
+            PC++;
+            break;
+        case 0x65:
+            mv(H,L);
+            PC++;
+            break;
+        case 0x75:
+            ad=b2u16(H,L);
+            mv(M[ad],L);
+            PC++;
+            break;
         case 6:
             // MVI
             mv(B,M[++PC]);
@@ -393,6 +505,25 @@ void exc(word *a)
         case 0x36:
             // MVI
             mv(M[b2u16(H,L)],M[++PC]);
+            PC++;
+            break;
+        case 0x46:
+            ad=b2u16(H,L);
+            mv(B,M[ad]);
+            PC++;
+            break;
+        case 0x56:
+            ad=b2u16(H,L);
+            mv(D,M[ad]);
+            PC++;
+            break;
+        case 0x66:
+            ad=b2u16(H,L);
+            mv(H,M[ad]);
+            PC++;
+            break;
+        case 0x76:
+            HLTF=1;
             PC++;
             break;
         case 7:
@@ -427,6 +558,39 @@ void exc(word *a)
         case 0x37:
             // STC
             FL[7]=1;
+            PC++;
+            break;
+        case 0x47:
+            mv(B,A);
+            PC++;
+            break;
+        case 0x57:
+            mv(D,A);
+            PC++;
+            break;
+        case 0x67:
+            mv(H,A);
+            PC++;
+            break;
+        case 0x77:
+            ad=b2u16(H,L);
+            mv(M[ad],A);
+            PC++;
+            break;
+        case 0x48:
+            mv(C,B);
+            PC++;
+            break;
+        case 0x58:
+            mv(E,B);
+            PC++;
+            break;
+        case 0x68:
+            mv(L,B);
+            PC++;
+            break;
+        case 0x78:
+            mv(A,B);
             PC++;
             break;
         case 9:
@@ -465,19 +629,64 @@ void exc(word *a)
         case 0x39:
             // DAD
             mv(oFL,FL);
-            _add16(SP,H,L,&c);
+            _add16(SP,H,L,&c,1);
             FL[0]=oFL[0];
             FL[1]=oFL[1];
             FL[3]=oFL[3];
             FL[5]=oFL[5];
             PC++;
             break;
+        case 0x59:
+            mv(E,C);
+            PC++;
+            break;
+        case 0x69:
+            mv(L,C);
+            PC++;
+            break;
+        case 0x79:
+            mv(A,C);
+            PC++;
+            break;
         case 0xA:
             // LDAX
-            mv(oFL,FL);
             ad=b2u16(B,C);
             mv(A,M[ad]);
-            mv(FL,oFL);
+            PC++;
+            break;
+        case 0x1A:
+            // LDAX
+            ad=b2u16(D,E);
+            mv(A,M[ad]);
+            PC++;
+            break;
+        case 0x2A:
+            // LHLD
+            ad=b2u16(M[PC+2],M[PC+1]);
+            mv(L,M[ad]);
+            mv(H,M[ad+1]);
+            PC+=3;
+            break;
+        case 0x3A:
+            // LDA
+            ad=b2u16(M[PC+2],M[PC+1]);
+            mv(A,M[ad]);
+            PC+=3;
+            break;
+        case 0x4A:
+            mv(C,D);
+            PC++;
+            break;
+        case 0x5A:
+            mv(E,D);
+            PC++;
+            break;
+        case 0x6A:
+            mv(L,D);
+            PC++;
+            break;
+        case 0x7A:
+            mv(A,D);
             PC++;
             break;
         case 0xB:
@@ -488,6 +697,41 @@ void exc(word *a)
             mv(FL,oFL);
             PC++;
             break;
+        case 0x1B:
+            // DCX
+            mv(oFL,FL);
+            _add(E,ONES,&c);
+            _add(D,ONES,&c);
+            mv(FL,oFL);
+            PC++;
+            break;
+        case 0x2B:
+            // DCX
+            mv(oFL,FL);
+            _add(L,ONES,&c);
+            _add(H,ONES,&c);
+            mv(FL,oFL);
+            PC++;
+            break;
+        case 0x3B:
+            // DCX
+            mv(oFL,FL);
+            _add16(SP,ONES,ONES,&c,0);
+            mv(FL,oFL);
+            PC++;
+            break;
+        case 0x4B:
+            mv(C,E);
+            PC++;
+            break;
+        case 0x6B:
+            mv(L,E);
+            PC++;
+            break;
+        case 0x7B:
+            mv(A,E);
+            PC++;
+            break;
         case 0xC:
             // INR
             mv(oFL,FL);
@@ -495,17 +739,121 @@ void exc(word *a)
             FL[7]=oFL[7];
             PC++;
             break;
+        case 0x1C:
+            // INR
+            mv(oFL,FL);
+            _add(E,ONE,&c);
+            FL[7]=oFL[7];
+            PC++;
+            break;
+        case 0x2C:
+            // INR
+            mv(oFL,FL);
+            _add(L,ONE,&c);
+            FL[7]=oFL[7];
+            PC++;
+            break;
+        case 0x3C:
+            // INR
+            mv(oFL,FL);
+            _add(A,ONE,&c);
+            FL[7]=oFL[7];
+            PC++;
+            break;
+        case 0x4C:
+            mv(C,H);
+            PC++;
+            break;
+        case 0x5C:
+            mv(E,H);
+            PC++;
+            break;
+        case 0x6C:
+            mv(L,H);
+            PC++;
+            break;
+        case 0x7C:
+            mv(A,H);
+            PC++;
+            break;
         case 0xD:
-            // DCR
             // DCR
             mv(oFL,FL);
             _add(C,ONES,&c);
             FL[7]=oFL[7];
             PC++;
             break;
+        case 0x1D:
+            // DCR
+            mv(oFL,FL);
+            _add(E,ONES,&c);
+            FL[7]=oFL[7];
+            PC++;
+            break;
+        case 0x2D:
+            // DCR
+            mv(oFL,FL);
+            _add(L,ONES,&c);
+            FL[7]=oFL[7];
+            PC++;
+            break;
+        case 0x3D:
+            // DCR
+            mv(oFL,FL);
+            _add(A,ONES,&c);
+            FL[7]=oFL[7];
+            PC++;
+            break;
+        case 0x4D:
+            mv(C,L);
+            PC++;
+            break;
+        case 0x5D:
+            mv(E,L);
+            PC++;
+            break;
+        case 0x7D:
+            mv(A,L);
+            PC++;
+            break;
         case 0xE:
             // MVI
             mv(C,M[++PC]);
+            PC++;
+            break;
+        case 0x1E:
+            // MVI
+            mv(E,M[++PC]);
+            PC++;
+            break;
+        case 0x2E:
+            // MVI
+            mv(L,M[++PC]);
+            PC++;
+            break;
+        case 0x3E:
+            // MVI
+            mv(A,M[++PC]);
+            PC++;
+            break;
+        case 0x4E:
+            ad=b2u16(H,L);
+            mv(C,M[ad]);
+            PC++;
+            break;
+        case 0x5E:
+            ad=b2u16(H,L);
+            mv(E,M[ad]);
+            PC++;
+            break;
+        case 0x6E:
+            ad=b2u16(H,L);
+            mv(L,M[ad]);
+            PC++;
+            break;
+        case 0x7E:
+            ad=b2u16(H,L);
+            mv(A,M[ad]);
             PC++;
             break;
         case 0xF:
@@ -520,8 +868,39 @@ void exc(word *a)
             FL[5]=oFL[5];
             PC++;
             break;
-        case 0x76:
-            HLTF=1;
+        case 0x1F:
+            // RAR
+            mv(oFL,FL);
+            FL[7]=A[7];
+            for(int i=WSIZE-1;i>0;i--) A[i]=A[i-1];
+            A[0]=oFL[7];
+            FL[0]=oFL[0];
+            FL[1]=oFL[1];
+            FL[3]=oFL[3];
+            FL[5]=oFL[5];
+            PC++;
+            break;
+        case 0x2F:
+            // CMA
+            for(int i=0;i<WSIZE;i++) A[i]^=1;
+            PC++;
+            break;
+        case 0x3F:
+            // CMC
+            FL[7]^=1;
+            PC++;
+            break;
+        case 0x4F:
+            mv(C,A);
+            PC++;
+            break;
+        case 0x5F:
+            mv(E,A);
+            PC++;
+            break;
+        case 0x6F:
+            mv(L,A);
+            PC++;
             break;
         default:
             break;
@@ -542,20 +921,18 @@ int main(int n, char **a)
 
     PC=100;
 
-    set(M[100],0x6);
-    set(M[101],0x3);
-    set(M[102],0xe);
-    set(M[103],0x2a);
-    set(M[104],0xa);
-
-    set(M[105],0x27);
+    set(M[100],0x2a);
+    set(M[101],0x2a);
+    set(M[102],0x3);
+    set(M[103],0x2b);
 
 
     set(M[110],0x76);
 
 
 
-    set(M[0x32a],0x9b);
+    set(M[0x32a],0);
+    set(M[0x32b],0x98);
 
     while(HLTF==0) {
         exc(M[PC]);
@@ -563,4 +940,6 @@ int main(int n, char **a)
 
     pw(A);
     pw(FL);
+    pw(H);
+    pw(L);
 }
